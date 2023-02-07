54bfdd9a83db798e24b57681df4e5909beb5704a
Nevir Wizurai <nevwizurai@gmail.com>
2023-02-07 09:10:36 +0700

    refactoring 'intro' section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Home from "./components/Home.section";
-import Intro from "./components/Intro.section";
 
 const App = () => {
   return (
     <main className="px-5">
       <Home />
-      <Intro />
     </main>
   );
 };
--- src/components/Home.section.jsx
+++ src/components/Home.section.jsx
@@ -50,6 +50,18 @@ const Home = () => {
           </a>
         </div>
       </div>
+
+      <div className="flex  flex-col gap-y-5 mt-10 px-5">
+        <h2 className="font-semibold text-xl underline text-center">A Little Bit About Myself</h2>
+        <p className="leading-5 text-justify tracking-tight italic">
+          My name is Nevir Wizurai Sabilillah, I am a frontend developer mostly proficient in React and tailwind, but I
+          do have a little experience with the backend as well.
+        </p>
+        <p className="leading-5 text-justify tracking-tight italic">
+          I am a passionate person when it comes to programming. I also consider myself a goal-oriented person, being
+          able to focus on one thing at a time, improving my overall effectiveness.
+        </p>
+      </div>
     </section>
   );
 };
