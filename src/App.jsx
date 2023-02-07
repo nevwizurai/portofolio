25144f88c38a9e60b1709f707d62da7469b982e1
Nevir Wizurai <nevwizurai@gmail.com>
2023-02-07 09:02:53 +0700

    added intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./components/Home.section";
+import Intro from "./components/Intro.section";
 
 const App = () => {
   return (
     <main className="px-5">
       <Home />
+      <Intro />
     </main>
   );
 };
