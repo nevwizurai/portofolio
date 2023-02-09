4e49a9a83ac4495e6e83820e45672c263afdc16b
Nevir Wizurai <nevwizurai@gmail.com>
2023-02-09 21:44:37 +0700

    adapting home section to all breakpoint

--- src/components/Home.section.jsx
+++ src/components/Home.section.jsx
@@ -5,7 +5,7 @@ import { AiFillLinkedin, AiFillFacebook } from "react-icons/ai";
 
 const Home = () => {
   return (
-    <section>
+    <section className="px-2">
       {/* Navbar */}
       <nav>
         <div className="flex justify-between items-center py-3">
@@ -19,48 +19,57 @@ const Home = () => {
         </div>
       </nav>
 
-      {/* Greeting */}
-      <div className="my-5">
-        {/* Text */}
-        <div className="text-center mb-5">
-          <small className="text-sm text-gray-500 italic">Hello, my name is</small>
-          <h2 className="text-xl font-bold leading-7">Nevir Wizurai Sabilillah</h2>
-          <p className="text-sm text-gray-500">Frontend developer</p>
+      {/* Main Container */}
+      <div className="lg:flex lg:justify-center">
+        {/* Greeting */}
+        <div className="my-5 px-2 lg:w-1/2">
+          {/* Text */}
+          <div className="text-center mb-5">
+            <small className="text-sm text-gray-500 italic">Hello, my name is</small>
+            <h2 className="text-xl font-bold leading-7">Nevir Wizurai Sabilillah</h2>
+            <p className="text-sm text-gray-500">Frontend developer</p>
+          </div>
+
+          {/* Picture */}
+          <div className="w-2/3 sm:w-1/2 mx-auto my-10 bg-gradient-to-br from-green-400 rounded-full overflow-hidden">
+            <img src={ImgMe} alt="my picture" className="max-w-full h-auto" />
+          </div>
+
+          {/* Social Links */}
+          <div className="flex justify-center items-center gap-10 text-4xl text-gray-500">
+            <a className=" hover:text-gray-800" href="https://github.com/nevwizurai" target={"_blank"}>
+              <FaGithubSquare />
+            </a>
+            <a className=" hover:text-gray-800" href="https://www.linkedin.com/in/nevwizurai" target={"_blank"}>
+              <AiFillLinkedin />
+            </a>
+            <a
+              className=" hover:text-gray-800"
+              href="https://www.facebook.com/profile.php?id=100082502372381"
+              target={"_blank"}
+            >
+              <AiFillFacebook />
+            </a>
+          </div>
         </div>
 
-        {/* Picture */}
-        <div className="w-2/3 mx-auto my-10 bg-gradient-to-br from-green-400 rounded-full overflow-hidden">
-          <img src={ImgMe} alt="my picture" className="max-w-full h-auto" />
+        <div className="flex  flex-col gap-y-5 mt-10 px-3 sm:w-3/4 sm:mx-auto lg:w-1/2">
+          <h2 className="font-semibold text-lg lg:text-xl bg-green-500 rounded-lg text-white text-center">
+            A Little Bit About Myself
+          </h2>
+          <p className="leading-6 lg:text-lg text-justify tracking-tight italic">
+            My name is Nevir Wizurai Sabilillah, I am a frontend developer{" "}
+            <span className="underline">mostly proficient</span> in{" "}
+            <span className="bg-blue-600 px-1 rounded py-0 text-white">React</span> and{" "}
+            <span className="bg-green-600 px-1 rounded py-0 text-white">Tailwind</span>, but I do have a little
+            experience with the backend as well.
+          </p>
+          <p className="leading-6 lg:text-lg text-justify tracking-tight italic">
+            I am a <span className="font-semibold">passionate</span> person when it comes to programming. I also
+            consider myself a <span className="font-semibold">goal-oriented</span> person, doing the best I can to
+            ensure the completion effectively.
+          </p>
         </div>
-
-        {/* Social Links */}
-        <div className="flex justify-center items-center gap-10 text-4xl text-gray-500">
-          <a className=" hover:text-gray-800" href="https://github.com/nevwizurai" target={"_blank"}>
-            <FaGithubSquare />
-          </a>
-          <a className=" hover:text-gray-800" href="https://www.linkedin.com/in/nevwizurai" target={"_blank"}>
-            <AiFillLinkedin />
-          </a>
-          <a
-            className=" hover:text-gray-800"
-            href="https://www.facebook.com/profile.php?id=100082502372381"
-            target={"_blank"}
-          >
-            <AiFillFacebook />
-          </a>
-        </div>
-      </div>
-
-      <div className="flex  flex-col gap-y-5 mt-10">
-        <h2 className="font-semibold text-xl underline text-center">A Little Bit About Myself</h2>
-        <p className="leading-5 text-justify tracking-tight italic">
-          My name is Nevir Wizurai Sabilillah, I am a frontend developer mostly proficient in React and tailwind, but I
-          do have a little experience with the backend as well.
-        </p>
-        <p className="leading-5 text-justify tracking-tight italic">
-          I am a passionate person when it comes to programming. I also consider myself a goal-oriented person, being
-          able to focus on one thing at a time, improving my overall effectiveness.
-        </p>
       </div>
     </section>
   );
