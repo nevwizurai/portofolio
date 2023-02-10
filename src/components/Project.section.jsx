871c41814c1f3fd48c4b5ee2fff4f314a8874622
Nevir Wizurai <nevwizurai@gmail.com>
2023-02-10 08:57:14 +0700

    adapting project section to all breakpoint

--- src/components/Project.section.jsx
+++ src/components/Project.section.jsx
@@ -34,45 +34,47 @@ const responsive = {
 
 const Project = () => {
   return (
-    <section className="">
-      <div className="mb-3">
-        <h2 className="font-medium text-lg leading-4">My Projects</h2>
-        <small className="italic text-gray-500">My past and currently work-in-progress projects...</small>
-      </div>
+    <section className="px-2 md:px-7">
+      <div className="xl:w-3/4 xl:mx-auto">
+        <div className="mb-3">
+          <h2 className="font-medium text-lg lg:text-2xl leading-4">My Projects</h2>
+          <small className="italic text-gray-500 lg:text-lg">My past and currently work-in-progress projects...</small>
+        </div>
 
-      {/* Cards container */}
-      <div>
-        <Carousel responsive={responsive} infinite={true} autoPlay={true} autoPlaySpeed={6000} showDots={true}>
-          <Item
-            image={img1}
-            name="Etch-a-Sketch"
-            desc="A miniature of etch-a-sketch game that lets user draw in a pixel-like layout"
-            repo_link={"https://github.com/nevwizurai/Etch-a-Sketch"}
-            live_link={"https://nevwizurai.github.io/Etch-a-Sketch"}
-            finished
-          />
-          <Item
-            image={img2}
-            name="Rock, Paper, Scissor"
-            desc="Play rock paper scissor against computer with this Javascript-based game"
-            repo_link={"https://github.com/nevwizurai/rock_paper_scissor"}
-            live_link={"https://nevwizurai.github.io/rock_paper_scissor"}
-            finished
-          />
-          <Item
-            image={img3}
-            name="Landing Page"
-            desc="A landing page website made using plain HTML, and CSS"
-            repo_link={"https://github.com/nevwizurai/landing_page"}
-            live_link={"https://nevwizurai.github.io/landing_page"}
-            finished
-          />
-          <Item
-            image={img4}
-            name="Student Management"
-            desc="Crud application to manage students data using MERN stack"
-          />
-        </Carousel>
+        {/* Cards container */}
+        <div className="px-5">
+          <Carousel responsive={responsive} infinite={true} autoPlay={true} autoPlaySpeed={6000} showDots={true}>
+            <Item
+              image={img1}
+              name="Etch-a-Sketch"
+              desc="A miniature of etch-a-sketch game that lets user draw in a pixel-like layout"
+              repo_link={"https://github.com/nevwizurai/Etch-a-Sketch"}
+              live_link={"https://nevwizurai.github.io/Etch-a-Sketch"}
+              finished
+            />
+            <Item
+              image={img2}
+              name="Rock, Paper, Scissor"
+              desc="Play rock paper scissor against computer with this Javascript-based game"
+              repo_link={"https://github.com/nevwizurai/rock_paper_scissor"}
+              live_link={"https://nevwizurai.github.io/rock_paper_scissor"}
+              finished
+            />
+            <Item
+              image={img3}
+              name="Landing Page"
+              desc="A landing page website made using plain HTML, and CSS"
+              repo_link={"https://github.com/nevwizurai/landing_page"}
+              live_link={"https://nevwizurai.github.io/landing_page"}
+              finished
+            />
+            <Item
+              image={img4}
+              name="Student Management"
+              desc="Crud application to manage students data using MERN stack"
+            />
+          </Carousel>
+        </div>
       </div>
     </section>
   );
@@ -80,13 +82,13 @@ const Project = () => {
 
 const Item = ({ image, name, desc, repo_link, live_link, finished }) => {
   return (
-    <div className="min-h-[200] rounded-xl shadow-mdoverflow-hidden p-1 bg-white border border-gray-200">
+    <div className="min-h-[200] w-full sm:w-2/3 lg:w-[95%] mx-auto rounded-xl shadow-mdoverflow-hidden p-1 bg-white border border-gray-200">
       <img className="object-cover object-top w-full h-48 rounded-xl" src={image} alt="project thumbnail" />
       <div className="p-4">
         <h4 className="text-xl font-semibold tracking-tight text-blue-600">{name}</h4>
         <p className="mb-2 leading-normal">{desc}</p>
         {finished ? (
-          <div className="flex gap-x-3">
+          <div className="flex gap-x-3 justify-center">
             <a href={repo_link} target="_blank" className="px-4 py-2 text-sm text-white bg-gray-800 rounded shadow">
               <AiOutlineGithub className="inline" /> View on Github
             </a>
